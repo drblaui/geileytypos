94a60656b1a0763680075eb9668b49398a049ef7
Alexander Rudolph <alexr30@web.de>
2022-03-18 22:24:31 +0100

    simple basic implementation

---
+++ Tweeter.py
@@ -0,0 +1,50 @@
+import tweepy, os
+from spellchecker import SpellChecker
+from dotenv import load_dotenv
+load_dotenv()
+
+class Tweeter:
+	def __init__(self, api_key, api_secret, access_token, access_secret):
+		auth = tweepy.OAuthHandler(api_key, api_secret)
+		auth.set_access_token(access_token, access_secret)
+		self.api = tweepy.API(auth)
+		self.own = "geileytypos"
+		self.target = "bendover1312"
+		self.checker = SpellChecker(case_sensitive=False)
+  
+	def tweet(self, text):
+		self.api.update_status(text)
+  
+	def retweet(self, target):
+		target.retweet()
+
+	def getLatestRetweet(self):
+		me = self.api.get_user(screen_name=self.own)
+		timeline = me.timeline()
+		for tweet in timeline:
+			if tweet.text.startswith("RT"):
+				return tweet
+		return None
+	
+	def containsError(self, tweet):
+		mentions = tweet.entities['user_mentions']
+		display_names = ['@' + mention['screen_name'] for mention in mentions]
+		tweet_text = tweet.text.replace("RT", "").replace(":","")
+		for name in display_names:
+			tweet_text = tweet_text.replace(name, "")
+		tweet_text = tweet_text.strip().split(" ")
+		errors = self.checker.unknown(tweet_text)
+		return len(errors) != 0
+
+	def search_timeline(self):
+		latest = self.getLatestRetweet()
+		timeline = self.api.get_user(screen_name=self.target).timeline()
+		for tweet in timeline:
+			if tweet.id == latest.retweeted_status.id:
+				break
+			if self.containsError(tweet):
+				tweet.retweet()
+
+
+this = Tweeter(os.getenv("API_KEY"), os.getenv("API_SECRET"), os.getenv("ACCESS_TOKEN"), os.getenv("ACCESS_SECRET"))
+this.search_timeline()
